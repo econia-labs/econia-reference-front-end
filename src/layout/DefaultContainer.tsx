42b96fa6938337bbbbe831a2badfeab2f6dc4b73
Brian Li <brnli7@gmail.com>
2022-12-19 12:39:29 -0500

    Fix website width

--- src/layout/DefaultContainer.tsx
+++ src/layout/DefaultContainer.tsx
@@ -4,6 +4,5 @@ import styled from "@emotion/styled";
  * Holds content and contains no padding.
  */
 export const DefaultContainer = styled.div`
-  max-width: 1300px;
   margin: 0;
 `;
--- src/layout/DefaultWrapper.tsx
+++ src/layout/DefaultWrapper.tsx
@@ -4,6 +4,5 @@ import styled from "@emotion/styled";
  * To be used to wrap a DefaultContainer. Has padding.
  */
 export const DefaultWrapper = styled.div`
-  width: 100%;
   padding: 0 36px;
 `;
