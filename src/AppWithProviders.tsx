51d0c030d80c642e4b28b731d26c39177175be61
Brian Li <brnli7@gmail.com>
2022-12-28 10:15:03 -0500

    Hide arrows in input

--- src/AppWithProviders.tsx
+++ src/AppWithProviders.tsx
@@ -76,4 +76,16 @@ const GlobalStyles = (theme: Theme) => css`
   p {
     margin: 0;
   }
+  // Hide arrows
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
 `;
