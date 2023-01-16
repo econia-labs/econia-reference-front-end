0a957ca9c950b321434fd9272a459334fc96bf52
Brian Li <brnli7@gmail.com>
2023-01-15 22:16:59 -0500

    Add additional top padding to Input to better center things inside

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ export const Input = styled.input`
   background-color: ${({ theme }) => theme.colors.grey[800]};
   border: ${({ theme }) => `1px solid ${theme.colors.grey[600]}`};
   color: ${({ theme }) => theme.colors.grey[100]};
-  padding: 14px 0px 14px 23px;
+  padding: 16px 0px 14px 23px;
   line-height: normal;
   outline: none;
   font-size: 16px;
