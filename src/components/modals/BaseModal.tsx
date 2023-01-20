4b113b8877f8b5dc0356c64f3f9d20671d6efac4
Brian Li <brnli7@gmail.com>
2023-01-20 10:41:29 -0500

    Make border purple of BaseModal when the X is hovered

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -12,6 +12,9 @@ export const BaseModal: React.FC<Modal.Props & PropsWithChildren> = ({
   children,
   ...props
 }) => {
+  const [xHover, setXHover] = React.useState(false);
+  console.log(xHover);
+
   let content = {};
   let overlay = {};
   let restStyle = {};
@@ -41,7 +44,10 @@ export const BaseModal: React.FC<Modal.Props & PropsWithChildren> = ({
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          border: `1px solid ${theme.colors.grey[600]}`,
+          transition: "all 300ms ease",
+          border: `1px solid ${
+            xHover ? theme.colors.purple.primary : theme.colors.grey[600]
+          }`,
           borderRadius: "0px",
           padding: "0px 72px",
           height: "fit-content",
@@ -57,7 +63,11 @@ export const BaseModal: React.FC<Modal.Props & PropsWithChildren> = ({
         ...restStyle,
       }}
     >
-      <CloseButtonContainer onClick={props.onRequestClose}>
+      <CloseButtonContainer
+        onClick={props.onRequestClose}
+        onMouseEnter={() => setXHover(true)}
+        onMouseLeave={() => setXHover(false)}
+      >
         <XIcon />
       </CloseButtonContainer>
       {children}
